e60623a5e2ec9dc6ef3565453a7df5084e150939
Danny Ebanks <56091180+danielEbanks@users.noreply.github.com>
2024-11-10 21:07:08 -0800

    update form

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzlqSH_pAT2R1lrP1cZJHhJ-z9X9QiC9_OkXgESsqnoF_0DUbgjUc9d6hh2szQ7sE5y/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzAGMb-nL0t7EdsRWaMs2xXKiK4cBWTeXPikqbTtYU36hvZ5wpjFxHUv_A9Y7bkYSVpqg/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
